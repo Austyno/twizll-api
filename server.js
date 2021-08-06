1645e51671d75038fceaad8470eb25594fa8f311
mac <mac@Austin-MacBook-Air.local>
2021-08-06 13:11:21 +0100

    created public folder

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectToDb()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, './views/static')))
+app.use(express.static(path.join(__dirname, './public')))
 app.use(fileUpload({ useTempFiles: true }))
 
 app.set('view engine', 'ejs')
@@ -78,7 +78,7 @@ app.use('/api/seller', sellerRoutes)
 app.use(Errors)
 
 const PORT = process.env.PORT || 5000
-const server = app.listen(
+app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,46 +1,46 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 const path = require('path')
-const { MailgunTransport } = require("mailgun-nodemailer-transport");
+const { MailgunTransport } = require('mailgun-nodemailer-transport')
 const transporter = nodemailer.createTransport(
   new MailgunTransport({
     auth: {
       domain: process.env.MAILGUN_DOMAIN,
-      apiKey:process.env.MAILGUN_API_KEY
+      apiKey: process.env.MAILGUN_API_KEY,
     },
   })
-);
+)
 
 class SendEmail {
   withTemplate = async (dataObject, email, templateSource, subject) => {
     return new Promise((resolve, reject) => {
-      const ejs = require('ejs');
-      let pathToTemplates = path.join(__dirname, '../views')
+      const ejs = require('ejs')
+      let pathToTemplates = path.join(__dirname, '../public/views')
       ejs.renderFile(
         path.join(pathToTemplates, templateSource),
         dataObject,
         function (err, data) {
           if (err) {
-            return reject(err);
+            return reject(err)
           } else {
             var mainOptions = {
               from: '"twizll" <austin@twizll.com>',
               to: email,
               subject,
               html: data,
-            };
+            }
 
             transporter.sendMail(mainOptions, function (err, info) {
               if (err) {
-                reject(err)}
-              else {
-                resolve(true);
+                reject(err)
+              } else {
+                resolve(true)
               }
-            });
+            })
           }
         }
-      );
-    });
-  };
+      )
+    })
+  }
 }
 
-module.exports = new SendEmail();
+module.exports = new SendEmail()
