9172782546e8306108c550f893b90cebbf1afb7f
mac <mac@Austin-MacBook-Air.local>
2021-08-17 12:32:44 +0100

    finished web hook to listen for subscription created

--- controllers/stripe/webHooksController.js
+++ controllers/stripe/webHooksController.js
@@ -9,22 +9,21 @@ const webHooks = async (req, res, next) => {
       req.body,
       req.headers['stripe-signature']
     )
-    
-    const data = event.data.object
 
+    const data = event.data.object
 
     switch (event.type) {
       case 'customer.subscription.created':
         //locate user and update info based on returned info from stripe after subscription
         const user = await User.findOne({ stripe_customer_id: data.customer })
 
-        // user.free_trial.status = 'completed'
-        // user.plan.status = data.status
-        // user.plan.type = data.plan.interval
-        // user.plan.start_date = moment(data.current_period_start)
-        // user.plan.end_date = moment(data.current_period_end)
-        // res.json({ received: true })
-        // await user.save()
+        user.free_trial.status = 'completed'
+        user.plan.status = data.status
+        user.plan.type = data.plan.interval
+        user.plan.start_date = moment(data.current_period_start)
+        user.plan.end_date = moment(data.current_period_end)
+        res.json({ received: true })
+        await user.save()
         console.log('user updated')
         break
       default:
